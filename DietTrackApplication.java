76a7e9ff27c05a4398a3a3534981840afb0c94f8
sara-ash98 <63973863+sara-ash98@users.noreply.github.com>
2024-10-16 14:43:05 +0530

    Add files via upload
    uploded src file

---
+++ DietTrackApplication.java
@@ -0,0 +1,13 @@
+package DietTrackingApp.DietTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietTrackApplication.class, args);
+	}
+
+}
